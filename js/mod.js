e93cb666208093a7ecf09b720faf10e4fc797dec
JerryMessi8 <jerrytyler.pinkerton@gmail.com>
2022-11-18 16:48:14 -0500

    Update mod.js
    The Minecraft Tree

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Minecraft Tree",
+	id: "TheModdingTreeMinecraft",
+	author: "JerryMessi8",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
