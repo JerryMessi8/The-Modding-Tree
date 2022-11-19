cdbe7750cd9fb2cc78aaf4b8cd6710e1313d1fc3
JerryMessi8 <jerrytyler.pinkerton@gmail.com>
2022-11-18 21:19:24 -0500

    Update layers.js
    more basics

--- js/layers.js
+++ js/layers.js
@@ -33,6 +33,7 @@ addLayer("p", {
             cost: new Decimal(1),
         
         },
+        unlocked() { return hasUpgrade("p", 11) },
         12: {
             title: "Break Tree",
             description: "Double your point gain.",
@@ -42,6 +43,7 @@ addLayer("p", {
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
+        unlocked() { return hasUpgrade("p", 12) },
         13: {
             title: "Craft Wood",
             description: "Double your point gain.",
@@ -51,6 +53,47 @@ addLayer("p", {
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
+        unlocked() { return hasUpgrade("p", 13) },
+        14: {
+            title: "Make Crafting Table",
+            description: "Double your point gain.",
+            cost: new Decimal(5),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
 
+    },
+})
+addLayer("p", {
+    name: "Start World", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#3b83c3",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "prestige points", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        let mult = new Decimal(1)
+        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
     },
 })
